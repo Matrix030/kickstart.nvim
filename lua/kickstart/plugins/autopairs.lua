2fd75fda8a9252a5f3c523512944dbcd111b2406
Matrix030 <rishigharat@gmail.com>
2025-07-29 09:53:39 -0400

    added golang and python lsp with autopairs for basic completions

--- lua/kickstart/plugins/autopairs.lua
+++ lua/kickstart/plugins/autopairs.lua
@@ -1,8 +1,22 @@
 -- autopairs
 -- https://github.com/windwp/nvim-autopairs
-
+-- ~/.config/nvim/lua/custom/plugins/autopairs.lua
 return {
-  'windwp/nvim-autopairs',
-  event = 'InsertEnter',
-  opts = {},
+  {
+    'windwp/nvim-autopairs',
+    event = 'InsertEnter',
+    opts = {
+      check_ts = true, -- enables treesitter for smarter pairing
+    },
+    config = function(_, opts)
+      require('nvim-autopairs').setup(opts)
+
+      -- If you use cmp (autocomplete), integrate with it
+      local cmp_status_ok, cmp = pcall(require, 'cmp')
+      if cmp_status_ok then
+        local cmp_autopairs = require 'nvim-autopairs.completion.cmp'
+        cmp.event:on('confirm_done', cmp_autopairs.on_confirm_done())
+      end
+    end,
+  },
 }
